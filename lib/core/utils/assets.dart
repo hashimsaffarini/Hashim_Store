ec20ce2a48176d06fb9765206620276d5aad7049
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-24 16:30:47 +0300

    fetch new feature : add Darwer..

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,4 +2,5 @@ class AssetsData {
   static const String logo = 'assets/images/splash.png';
   static const String location = 'assets/icons/location.svg';
   static const String profileImage = 'assets/images/profile.png';
+  static const String background = 'assets/images/background.png';
 }
--- lib/features/home/presentation/views/widgets/home_page_app_bar.dart
+++ lib/features/home/presentation/views/widgets/home_page_app_bar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hashim_store/core/utils/app_color.dart';
 import 'package:hashim_store/core/utils/assets.dart';
+
 class HomePageAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomePageAppBar({
     super.key,
@@ -12,29 +12,19 @@ class HomePageAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.white,
-      leading: Container(
-        padding: const EdgeInsets.only(left: 22),
-        child: SvgPicture.asset(
-          AssetsData.location,
-          colorFilter: const ColorFilter.mode(
-            AppColors.primaryColor,
-            BlendMode.srcIn,
-          ),
-        ),
-      ),
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'Location',
+            'Hi, Welcome Back',
             style: GoogleFonts.inter(
-              fontSize: 18,
+              fontSize: 16,
               fontWeight: FontWeight.w400,
               color: AppColors.greyPrimary,
             ),
           ),
           Text(
-            'Palestine, Tulkarm',
+            'Go Shopping Now!',
             style: GoogleFonts.inter(
               fontSize: 20,
               fontWeight: FontWeight.w500,
