e97eac2286b49f23665e9c343d6bec84ce798d7e
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-24 16:32:32 +0300

    refactor drawer code

--- lib/features/home/presentation/views/pages/home_page.dart
+++ lib/features/home/presentation/views/pages/home_page.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:hashim_store/core/utils/assets.dart';
 import 'package:hashim_store/features/home/presentation/views/widgets/home_page_app_bar.dart';
 import 'package:hashim_store/features/home/presentation/views/widgets/home_page_body.dart';
+import 'package:hashim_store/features/home/presentation/views/widgets/home_page_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const HomePageAppBar(),
-      body: const SafeArea(
+    return const Scaffold(
+      appBar: HomePageAppBar(),
+      body: SafeArea(
         child: Column(
           children: [
             SizedBox(height: 40),
@@ -20,93 +18,7 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      drawer: Drawer(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          ),
-        ),
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text(
-                'Hashim Store🛍️ ',
-                style: TextStyle(
-                  fontFamily: GoogleFonts.inter().fontFamily,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              accountEmail: const Text(
-                'Palestine, Tulkarm🇵🇸',
-                style: TextStyle(
-                  fontSize: 13,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              currentAccountPicture: const CircleAvatar(
-                backgroundImage: AssetImage(AssetsData.logo),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                'Contact Us :',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 12),
-            ListTile(
-              leading: Icon(
-                FontAwesomeIcons.facebook,
-                color: Colors.blue[800], // Facebook's iconic blue color
-              ),
-              title: const Text(
-                'Facebook',
-              ),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(
-                FontAwesomeIcons.instagram,
-                color: Colors.pink,
-              ),
-              title: const Text('Instagram'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(
-                FontAwesomeIcons.whatsapp,
-                color: Colors.green,
-              ),
-              title: const Text('WhatsApp'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(
-                FontAwesomeIcons.phone,
-                color: Colors.blue,
-                size: 20,
-              ),
-              title: const Text('Phone'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(
-                FontAwesomeIcons.locationArrow,
-                color: Colors.red,
-              ),
-              title: const Text('Location'),
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
+      drawer: HomePageDrawer(),
     );
   }
 }
