6509fff97187b975fffd5d1098ef923cec937ad3
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-27 00:40:12 +0300

    Update product details page UI and add product details app bar widget

--- lib/features/home/presentation/views/pages/product_details_page.dart
+++ lib/features/home/presentation/views/pages/product_details_page.dart
@@ -1,24 +1,50 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:hashim_store/core/utils/app_color.dart';
 import 'package:hashim_store/features/home/data/models/product_item_model.dart';
+import 'package:hashim_store/features/home/presentation/views/widgets/product_details_app_bar.dart';
 
 class ProductDetailsPage extends StatelessWidget {
   const ProductDetailsPage({super.key, required this.product});
   final ProductItemModel product;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ProductDetailsPageBody(),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: const ProductDetailsPageAppBar(),
+      body: SafeArea(
+        top: false,
+        child: ProductDetailsPageBody(
+          product: product,
+        ),
+      ),
     );
   }
 }
 
 class ProductDetailsPageBody extends StatelessWidget {
-  const ProductDetailsPageBody({super.key});
-
+  const ProductDetailsPageBody({super.key, required this.product});
+  final ProductItemModel product;
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: size.height * 0.6,
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset(
+              product.imgUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
---
+++ lib/features/home/presentation/views/widgets/product_details_app_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hashim_store/core/utils/app_color.dart';
+
+class ProductDetailsPageAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const ProductDetailsPageAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      flexibleSpace: LayoutBuilder(
+        builder: (context, constraints) {
+          return Padding(
+            padding: const EdgeInsets.only(top: 60, left: 24),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                    size: 26,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(0, 2),
+                        blurRadius: 4.0,
+                        color: Color.fromRGBO(0, 0, 0, 0.25),
+                      )
+                    ],
+                  ),
+                  onPressed: () {
+                    GoRouter.of(context).pop();
+                  },
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(120);
+}
