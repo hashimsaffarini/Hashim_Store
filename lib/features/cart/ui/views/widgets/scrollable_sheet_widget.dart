4995ba98c0ab26ce18c0147b028682d29f04a7ce
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-02 19:25:20 +0300

    Refactor initialChildSize, minChildSize, and maxChildSize in ScrollableSheetWidget

--- lib/features/cart/ui/views/widgets/scrollable_sheet_widget.dart
+++ lib/features/cart/ui/views/widgets/scrollable_sheet_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hashim_store/features/cart/ui/views/widgets/single_child_scroll_child_cart_page.dart';
+
 class ScrollableSheetWidget extends StatelessWidget {
   const ScrollableSheetWidget({
     super.key,
@@ -8,9 +9,9 @@ class ScrollableSheetWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
-      initialChildSize: 0.39,
-      minChildSize: 0.1,
-      maxChildSize: 0.42,
+      initialChildSize: 0.35,
+      minChildSize: 0.03,
+      maxChildSize: 0.39,
       builder: (context, scrollController) {
         return Container(
           decoration: customBoxDecoration(),
@@ -25,19 +26,19 @@ class ScrollableSheetWidget extends StatelessWidget {
 
   BoxDecoration customBoxDecoration() {
     return BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(24),
-            topRight: Radius.circular(24),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 1,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        );
+      color: Colors.white,
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(24),
+        topRight: Radius.circular(24),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 1,
+          blurRadius: 5,
+          offset: const Offset(0, 3),
+        ),
+      ],
+    );
   }
 }
