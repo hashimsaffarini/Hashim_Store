780f32a9c69eabc50db06651530f509bc7ee7ec7
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-03 16:34:44 +0300

    refactor code of cart page

--- lib/features/cart/ui/views/widgets/add_and_remove_widget.dart
+++ lib/features/cart/ui/views/widgets/add_and_remove_widget.dart
@@ -1,9 +1,7 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hashim_store/core/utils/app_color.dart';
 import 'package:hashim_store/features/cart/ui/logic/cart_cubit/cart_page_cubit.dart';
+import 'package:hashim_store/features/cart/ui/views/widgets/counter_widget.dart';
 import 'package:hashim_store/features/home/data/models/product_item_model.dart';
 
 class AddAndRemoveWidget extends StatelessWidget {
@@ -33,84 +31,6 @@ class AddAndRemoveWidget extends StatelessWidget {
           );
         }
       },
-    );
-  }
-}
-
-class CounterWidget extends StatelessWidget {
-  const CounterWidget({
-    super.key,
-    required this.product,
-    required this.cubit,
-    required this.counter,
-  });
-  final dynamic cubit;
-  final ProductItemModel product;
-  final int counter;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        GestureDetector(
-          onTap: () {
-            log('Remove');
-            cubit.decrement(product);
-          },
-          child: Container(
-            height: 40,
-            width: 40,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 223, 217, 217),
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
-              ),
-            ),
-            child: const Icon(
-              Icons.remove,
-              color: AppColors.primaryColor,
-              size: 20,
-            ),
-          ),
-        ),
-        const SizedBox(width: 12),
-        Text(
-          '$counter',
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        const SizedBox(width: 12),
-        GestureDetector(
-          onTap: () {
-            log('Add');
-            cubit.increment(product);
-          },
-          child: Container(
-            height: 40,
-            width: 40,
-            decoration: const BoxDecoration(
-              color: AppColors.primaryColor,
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
-              ),
-            ),
-            child: const Icon(
-              Icons.add,
-              color: Colors.white,
-              size: 20,
-            ),
-          ),
-        ),
-        const Spacer(),
-        Text(
-          '₪${product.price}',
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/features/cart/ui/views/widgets/counter_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:hashim_store/core/utils/app_color.dart';
+import 'package:hashim_store/features/home/data/models/product_item_model.dart';
+class CounterWidget extends StatelessWidget {
+  const CounterWidget({
+    super.key,
+    required this.product,
+    required this.cubit,
+    required this.counter,
+  });
+  final dynamic cubit;
+  final ProductItemModel product;
+  final int counter;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: () {
+            cubit.decrement(product);
+          },
+          child: Container(
+            height: 40,
+            width: 40,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 223, 217, 217),
+              borderRadius: BorderRadius.all(
+                Radius.circular(50),
+              ),
+            ),
+            child: const Icon(
+              Icons.remove,
+              color: AppColors.primaryColor,
+              size: 20,
+            ),
+          ),
+        ),
+        const SizedBox(width: 12),
+        Text(
+          '$counter',
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        const SizedBox(width: 12),
+        GestureDetector(
+          onTap: () {
+            cubit.increment(product);
+          },
+          child: Container(
+            height: 40,
+            width: 40,
+            decoration: const BoxDecoration(
+              color: AppColors.primaryColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(50),
+              ),
+            ),
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 20,
+            ),
+          ),
+        ),
+        const Spacer(),
+        Text(
+          '₪${(product.price * product.sale) / 100}',
+          style: const TextStyle(
+             fontSize: 18,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
+    );
+  }
+}
