f447253c24106847777512e3098c852345906a5b
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-05 11:33:38 +0300

    feat: Add Firebase initialization and import necessary packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hashim_store/core/utils/app_color.dart';
 import 'package:hashim_store/core/utils/app_router.dart';
-import 'package:hashim_store/core/utils/bloc_observer.dart';
+import 'package:hashim_store/firebase_options.dart';
 
-void main() {
-  Bloc.observer = SimpleBlocObserver();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
