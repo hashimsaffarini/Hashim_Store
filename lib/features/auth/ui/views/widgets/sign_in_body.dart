d808f9c139680d1f6393a045d682e22e7b30c910
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-05-06 23:30:45 +0300

    chore: Refactor sign-in page UI and add sign-up functionality

--- lib/features/auth/ui/views/widgets/sign_in_body.dart
+++ lib/features/auth/ui/views/widgets/sign_in_body.dart
@@ -12,54 +12,56 @@ class SignInBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 80),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SignInHeader(),
-          const SizedBox(height: 60),
-          const SignInForm(),
-          const SizedBox(height: 46),
-          CustomButton(
-            onPressed: () {},
-            borderRadius: 36,
-            child: Text(
-              'Sign In',
-              style: GoogleFonts.interTight(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Don\'t have an account?',
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SignInHeader(),
+            const SizedBox(height: 60),
+            const SignInForm(),
+            const SizedBox(height: 46),
+            CustomButton(
+              onPressed: () {},
+              borderRadius: 36,
+              child: Text(
+                'Sign In',
                 style: GoogleFonts.interTight(
-                  fontSize: 16,
+                  fontSize: 20,
                   fontWeight: FontWeight.w500,
-                  color: Colors.black,
+                  color: Colors.white,
                 ),
               ),
-              const SizedBox(width: 2),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Sign Up',
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Don\'t have an account?',
                   style: GoogleFonts.interTight(
                     fontSize: 16,
                     fontWeight: FontWeight.w500,
-                    color: AppColors.primaryColor,
-                    decoration: TextDecoration.underline,
-                    decorationColor: AppColors.primaryColor,
+                    color: Colors.black,
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(width: 2),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Sign Up',
+                    style: GoogleFonts.interTight(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.primaryColor,
+                      decoration: TextDecoration.underline,
+                      decorationColor: AppColors.primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
